97ec932e91ee0289d1d08f70d897b247e8d51e7d
brettrsa <brettrsa@gmail.com>
2015-05-02 19:08:11 +0530

    Update get-ip-info.sh

--- get-ip-info.sh
+++ get-ip-info.sh
@@ -2,13 +2,15 @@
 #
 # Start
 
-if [ ! $1 ];then
-      echo -e "Usage: "
-      echo -e "./ip-info <ip address>"
-      echo -e "./ip-info me - Info about your source public ip address"
-      exit
-fi
 
-curl -s http://dazzlepod.com/ip/$1.json | tr "," \\n | sed 's/"//g' | sed 's/{//g' | sed 's/}//g'
+get_ip_info()
+{
+    echo '---------------------------------------'
+    echo 'Grabbing info..........................'
+    ip=$(curl -s http://api.ipify.org)
+    curl -s http://dazzlepod.com/ip/$ip.json | tr "," \\n | sed 's/"//g' | sed 's/{//g' | sed 's/}//g'
+}
+
+get_ip_info
 
 # End
